49114004bda17fef52bd96b57e850ef513da0118
Ben Drucker <bvdrucker@gmail.com>
2015-12-11 07:51:56 -0800

    Add tests for initial render

--- test.js
+++ test.js
@@ -1,8 +1,21 @@
 'use strict'
 
 var test = require('tape')
-var circleBounce = require('./')
+var $ = require('vdom-query')
+var partial = require('ap').partial
+var Circles = require('./')
 
-test(function (t) {
+test('initial render', function (t) {
+  var circles = Circles({radius: 10})
 
+  var elements = $(partial(circles, 0)).find('circle')
+
+  t.equal(svgAttr(elements.eq(0), 'r'), 0, 'mask radius = 0')
+  t.equal(svgAttr(elements.eq(1), 'r'), 10, 'outer radius = 10')
+
+  t.end()
 })
+
+function svgAttr (vquery, key) {
+  return vquery.get(0).properties.attributes[key]
+}
